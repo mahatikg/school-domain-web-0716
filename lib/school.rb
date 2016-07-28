78e18882851a88a51d88904abb4c4a251f41f677
Mahati Gollamudi <github email address>
2016-07-28 09:10:31 -0400

    Add Grades and Students into Array based on what information is present

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,42 @@
 # code here!
+require 'pry'
+
+class School
+  attr_accessor :roster
+  attr_reader :name
+
+
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+
+  end
+
+
+  def add_student(name, grade)
+
+      if @roster[grade].nil?
+    #is .nil? actually a method or is the '?' a method?
+    #how exactly does this question mark thing work?
+    #binding.pry
+        @roster[grade] = []
+        @roster[grade] << name
+      else
+        @roster[grade] << name 
+      end
+
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, name_array|
+      @roster[grade] = name_array.sort
+    end
+    @roster
+  end
+
+end
